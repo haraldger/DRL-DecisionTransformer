81d8e0f1aa4cd53058cba622784a3af03651668a
haraldger <harald.gerhardsen@hotmail.com>
2023-04-06 18:50:51 -0400

    Implement most of masked self attention heads. Missing: apply output weights for all attention heads.

--- DecisionTransformer/model.py
+++ DecisionTransformer/model.py
@@ -15,30 +15,56 @@ class MaskedAttentionHead(nn.Module):
         self.embedding_dim = embedding_dim
 
         # Weights for keys, queries and values, in a batch
-        self.w_attention = nn.Linear(embedding_dim, 3*embedding_dim)
-        self.softmax = nn.Softmax(dim=1)
+        self.w_attention = nn.Linear(embedding_dim, num_heads*3*embedding_dim)
+        self.softmax = nn.Softmax(dim=-1)
+        # self.w_output = nn.Linear()
 
     def forward(self, x):
         """
-        x - (seq_length x embedding_dim)
+        x - (batch_size x seq_length x embedding_dim)
+        bs - batch size
+        sql - sequence length
+        edim - embedding dimension  (self.embedding_dim)
+        nh - number of attention heads    (self.num_heads)
         """
 
         # Linearly project onto the learnt subspace.
-        Q = self.w_q(x)     # (seq_length x k_dim)
-        K = self.w_k(x)     # (seq_length x k_dim)
-        V = self.w_v(x)     # (seq_length x v_dim)
+        projections = self.w_attention(x)   # (bs x sql x edim) -> (bs x sql x nh*3*edim)
+        projections = torch.split(projections, self.embedding_dim * self.num_heads, dim=-1)
 
-        # Attention
-        compatibility = Q @ K.T     # (seq_length x seq_length)
-        scaled_compatibility = torch.divide(compatibility, math.sqrt(self.k_dim))
+        # Q, K, V is each (bs x sql x (nh * edim))
+        Q, K, V = projections
+
+        # Re-shape each of Q,K,V to (bs x nh x sql x edim)
+        Q = Q.contiguous().view((-1, self.num_heads, x.shape[1], self.embedding_dim))
+        K = K.contiguous().view((-1, self.num_heads, x.shape[1], self.embedding_dim))
+        V = V.contiguous().view((-1, self.num_heads, x.shape[1], self.embedding_dim))
+
+
+        # # Attention
+        # (bs x nh x sql x edim) * (bs x nh x edim x sql) -> (bs x nh x sql x sql)
+        compatibility = Q @ torch.transpose(K, -1, -2)   
+        scaled_compatibility = torch.divide(compatibility, math.sqrt(self.embedding_dim))
         attention_scores = self.softmax(scaled_compatibility)
-        output = attention_scores @ V   # (seq_length x v_dim)
 
-        return output
+        # Output
+        output = attention_scores @ V   # (bs x nh x sql x sql) * (bs x nh x sql x edim) -> (bs x nh x sql x edim)
+        # TODO: apply attention head weights
+        print(output.shape)
+
+        return None
 
 
 class DecisionTransformer(nn.Module):
     def __init__(self) -> None:
         super().__init__()
 
+bs = 1
+seq_len = 10
+n_heads = 3
+e_dim = 7
+
+net = MaskedAttentionHead(n_heads, e_dim)
+x = torch.rand(bs, seq_len, e_dim)
+x = net(x)
 
