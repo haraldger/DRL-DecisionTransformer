bcb76b259974aad64dc5de36f88b07a5b5747766
David Palumbo <djpalumb@buffalo.edu>
2023-05-01 19:38:31 -0400

    GPT block for DT

--- Agents/dt_agent.py
+++ Agents/dt_agent.py
@@ -4,8 +4,8 @@ import torch
 from torch import nn
 from .agent import Agent
 
-class MaskedAttentionHead(nn.Module):
-    def __init__(self, num_heads, embedding_dim, *args, **kwargs) -> None:
+class AttentionHead(nn.Module):
+    def __init__(self, num_heads, embedding_dim, masked=False, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         """
@@ -19,6 +19,8 @@ class MaskedAttentionHead(nn.Module):
         self.w_attention = nn.Linear(embedding_dim, num_heads * 3 * embedding_dim)
         self.softmax = nn.Softmax(dim=-1)
         self.w_output = nn.Linear(num_heads * embedding_dim, embedding_dim)
+
+        self.masked = masked
 
     def forward(self, x):
         """
@@ -41,15 +43,18 @@ class MaskedAttentionHead(nn.Module):
         K = K.contiguous().view((-1, self.num_heads, x.shape[1], self.embedding_dim))
         V = V.contiguous().view((-1, self.num_heads, x.shape[1], self.embedding_dim))
 
-
         # Attention
         # (bs x nh x sql x edim) * (bs x nh x edim x sql) -> (bs x nh x sql x sql)
         compatibility = Q @ torch.transpose(K, -1, -2)
         scaled_compatibility = torch.divide(compatibility, math.sqrt(self.embedding_dim))
-        mask = torch.ones_like(scaled_compatibility) * float('-inf')
-        mask = torch.triu(mask, 1)
-        masked_compatibility = scaled_compatibility + mask
-        attention_scores = self.softmax(masked_compatibility)
+        
+        if self.masked:
+            mask = torch.ones_like(scaled_compatibility) * float('-inf')
+            mask = torch.triu(mask, 1)
+            masked_compatibility = scaled_compatibility + mask
+            attention_scores = self.softmax(masked_compatibility)
+        else:
+            attention_scores = self.softmax(scaled_compatibility)
 
         # Output
         output = attention_scores @ V   # (bs x nh x sql x sql) * (bs x nh x sql x edim) -> (bs x nh x sql x edim)
@@ -58,6 +63,45 @@ class MaskedAttentionHead(nn.Module):
 
         return output
 
+
+class GPTBlock(nn.Module):
+    def __init__(self, num_heads, embedding_dim, ff_dim, dropout, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.attention_block = AttentionHead(num_heads, embedding_dim, args, kwargs)
+        self.ln1 = nn.LayerNorm(embedding_dim)
+        
+        # Feed forward network
+        self.feedforward = nn.Sequential(
+            nn.Linear(embedding_dim, ff_dim),
+            nn.ReLU,
+            nn.Linear(ff_dim, embedding_dim),
+            nn.Dropout(dropout)
+        )
+
+        self.ln2 = nn.LayerNorm(embedding_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # Forward through the masked multi-head
+        attn_out = self.attention_block.forward(x)
+
+        # Skip connection
+        x = self.dropout(attn_out) + x
+
+        # Layer Norm
+        x = self.ln1(x)
+
+        # Feed forward 
+        ff_out = self.feedforward(x)
+        
+        # Skip conncetion 
+        x = ff_out + x
+
+        # Layer Norm
+        x = self.ln2(x)
+
+        return x
+        
 
 class DecisionTransformer(nn.Module):
     def __init__(self) -> None:
