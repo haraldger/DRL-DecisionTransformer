a7079dae221ba1bfec78c400355f0c3b5fa093b9
haraldger <harald.gerhardsen@hotmail.com>
2023-05-06 18:24:30 -0400

    Restructure test execution

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,9 @@
 from tests import data_collection_test, resnet_test, dqn_test, dt_agent_test
 
 def run():
-    dt_agent_test.run()
     data_collection_test.run()
     resnet_test.run()
+    dt_agent_test.run()
     dqn_test.run()
 
 if __name__ == '__main__':
